a90295689b3690221cb47f8fa3bf360a242f63d9
SibMan54 <ilya89vet@gmail.com>
2025-01-31 18:56:44 +0700

    Upgrade function "incoming_call_sms"

--- Arduino_GSM_Relay.ino
+++ Arduino_GSM_Relay.ino
@@ -2,13 +2,13 @@
 Проект GSM реле на Arduino с использованием модуля NEOWAY M590
 Проект был написан давно, но небыл никуда опубликован, лежал на ПК
 
-Функционал реле следующий:  
-1. Реле вкл и выкл нагрузку по звонку, при звонке на устройство проискодит сброс вызова и смена состояния реле ВКЛ/ВЫКЛ;  
-2. Включение и выключение реле через SMS, отправляем смс на устройство с текстом "relay on" "relay off" в ответ получаем смс о статусе;  
-3. Включение с таймером через SMS, отправляем смс на устройство с текстом "timer МИН" например "timer 60" включит реле и через 60 мин сам выкл его;  
-4. Проверка температуры с датчика DS18b20, отправляем смс на устройство с текстом "temper" в ответ получаем смс с текущей температурой;  
+Функционал реле следующий:
+1. Реле вкл и выкл нагрузку по звонку, при звонке на устройство проискодит сброс вызова и смена состояния реле ВКЛ/ВЫКЛ;
+2. Включение и выключение реле через SMS, отправляем смс на устройство с текстом "relay on" "relay off" в ответ получаем смс о статусе;
+3. Включение с таймером через SMS, отправляем смс на устройство с текстом "timer МИН" например "timer 60" включит реле и через 60 мин сам выкл его;
+4. Проверка температуры с датчика DS18b20, отправляем смс на устройство с текстом "temper" в ответ получаем смс с текущей температурой;
 5. Также я попытался реализовать возможность самоподогрева устройства отправкой SMS с текстом "termostat ТЕМПЕРАТУРА ВКЛ ПОДОГРЕВА"
-например "termostat -10" если датчик стоит внутри устройства, то при достижении -10 град. вкл самоподогрев;  
+например "termostat -10" если датчик стоит внутри устройства, то при достижении -10 град. вкл самоподогрев;
 6. И самое главное в устройстве реализована защита от сторонних звонков и СМС, в устройстве сохраняются 2 номера тел MASTER и MASTER2 с которых можно управлять устройством.
 Имеется возможность смены этих номеров при помощи СМС, для этого отправляем смс с текстом "new master" или "new master2" с номера который вы хотите сделать мастер номером,
 то есть если вы хотите номер 123456789 сделать первым, то отправляем с него текст "new master" а если хотите сделать его вторым, то отправляете с него смс "new master2";
@@ -90,7 +90,7 @@ void setup()
   digitalWrite(LED, state);
   pinMode(heater, OUTPUT);
   pinMode(BUTTON, INPUT_PULLUP);               //конфигурация кнопки на вход
-  
+
   //Serial.begin(9600);
   mySerial.begin(9600);                //подключаем порт модема
   sendAtCmd("AT+IPR?");                // запрос у модема скорости
@@ -109,7 +109,7 @@ void setup()
   delay(300);
   mySerial.println("AT+CNMI=2,2");     //отображение смс в терминале сразу после приема (без этого сообщения молча падают в память)
   delay(300);
-  
+
   while (!mySerial.find("+PBREADY")) {
     mySerial.println("AT+CSQ");          //вывести в терминал уровень сигнала (если 99, то связи нет)
     delay(300);
@@ -131,9 +131,9 @@ void setup()
   if (read_master2_eeprom(30).indexOf("79") > -1 && read_master2_eeprom(30).length() == 11) {
     MASTER2 = read_master2_eeprom(30);
   }
-  
+
   digitalWrite(powLED,HIGH);
-  
+
   //Serial.print("MASTER №: ");
   //Serial.println(MASTER);
   //Serial.print("MASTER №2: ");
@@ -195,7 +195,7 @@ void readNumberSIM() {
    if (val.indexOf("79") > -1 && val.length() == 11) {
      MASTER = val;
    }
-   
+
    ch = 0;
    val = "";
    update_master_eeprom(10);
@@ -209,7 +209,7 @@ String read_master_eeprom( int adr) {
     String number = "";
     for ( byte i = 0; i < 12; i++) EEPROM.get( adr + i, number[i] );
     return number;
-} 
+}
 String read_master2_eeprom( int adr) {
     String number = "";
     for ( byte i = 0; i < 12; i++) EEPROM.get( adr + i, number[i] );
@@ -232,8 +232,8 @@ float currentTemper() {
     sensDs.write(0x44, DS_POWER_MODE); // инициализация измерения
     delay(900);  // пауза 0,9 сек
     sensDs.reset();  // сброс шины
-    sensDs.write(0xCC, DS_POWER_MODE); // пропуск ROM 
-    sensDs.write(0xBE, DS_POWER_MODE); // команда чтения памяти датчика 
+    sensDs.write(0xCC, DS_POWER_MODE); // пропуск ROM
+    sensDs.write(0xBE, DS_POWER_MODE); // команда чтения памяти датчика
     sensDs.read_bytes(bufData, 9);  // чтение памяти датчика, 9 байтов
     if ( OneWire::crc8(bufData, 8) == bufData[8] ) {  // проверка CRC
       // если данные правильные
@@ -243,12 +243,15 @@ float currentTemper() {
 }
 
 
+//-----------------------------------------------------------------------------
+// Цикл loop
+//-----------------------------------------------------------------------------
 void loop()
 {
   if(digitalRead(BUTTON)==LOW) btnCheck();          //есть была нажата кнопка
-  
+
   if (mySerial.available()) incoming_call_sms();          //есть данные от GSM модуля
-  
+
   #ifdef USE_TERMOSTAT
   if(heaterVal != 0 && millis()-curTime>30000) {
     if(currentTemper() <= heaterVal && tmpFlag == false) {
@@ -282,178 +285,138 @@ void loop()
 //-----------------------------------------------------------------------------
 //процедура обработки звонка и смс
 //-----------------------------------------------------------------------------
-void incoming_call_sms()
-{
-  byte ch = 0;
-  delay(200);
-  while (mySerial.available()) {         //сохраняем входную строку в переменную val
-    ch = mySerial.read();
-    val += char(ch);
-    delay(10);
-  }
-  //----------------------- определение факта приема СМС и сравнение номера с заданным
-  if (val.indexOf("+CMT") > -1)           //если обнаружен СМС (для определения звонка вместо "+CMT" вписать "RING", трубку он не берет, но реагировать на факт звонка можно)
-  {
+enum Command {
+    CMD_DELETE_SMS,
+    CMD_RELAY_ON,
+    CMD_RELAY_OFF,
+    CMD_TIMER,
+    CMD_TEMPERATURE,
+    CMD_TERMOSTAT,
+    CMD_NEW_MASTER,
+    CMD_NEW_MASTER2,
+    CMD_SIM_MASTER,
+    CMD_RING,
+    CMD_UNKNOWN
+};
 
-    // для очистки памяти от старых смс-ок, очистка есть при перезапуске, но скидывать клему не хочется
-    if ((val.indexOf(MASTER) > -1 || val.indexOf(MASTER2) > -1) && val.indexOf("delete sms") > -1) //если СМС от хозяина и содержит текст запроса
-    {
-      if (val.indexOf(MASTER) > -1) sms("Delete SMS OK", MASTER); // отвечаем смской
-      else sms("Delete SMS OK", MASTER2); // отвечаем смской
-      val = "";
-      delay(1000);
-      sendAtCmd("AT+CMGD=1,4");
+Command getCommand(const String& val) {
+    if (val.indexOf("+CMT") > -1) {
+        if ((val.indexOf(MASTER) > -1 || val.indexOf(MASTER2) > -1) && val.indexOf("delete sms") > -1) return CMD_DELETE_SMS;
+        if ((val.indexOf(MASTER) > -1 || val.indexOf(MASTER2) > -1) && val.indexOf("relay on") > -1 && !state) return CMD_RELAY_ON;
+        if ((val.indexOf(MASTER) > -1 || val.indexOf(MASTER2) > -1) && val.indexOf("relay off") > -1 && state) return CMD_RELAY_OFF;
+        if ((val.indexOf(MASTER) > -1 || val.indexOf(MASTER2) > -1) && val.indexOf("timer ") > -1) return CMD_TIMER;
+        if ((val.indexOf(MASTER) > -1 || val.indexOf(MASTER2) > -1) && val.indexOf("temper") > -1) return CMD_TEMPERATURE;
+        if ((val.indexOf(MASTER) > -1 || val.indexOf(MASTER2) > -1) && val.indexOf("termostat ") > -1) return CMD_TERMOSTAT;
+        if (val.indexOf("new master") > -1) return CMD_NEW_MASTER;
+        if (val.indexOf("new master2") > -1) return CMD_NEW_MASTER2;
+        if (val.indexOf("SIM master N") > -1) return CMD_SIM_MASTER;
     }
-    else if ((val.indexOf(MASTER) > -1 || val.indexOf(MASTER2) > -1) && val.indexOf("relay on") > -1 && state == false) //если СМС от хозяина и содержит текст запроса
-    {
-      digitalWrite(power, HIGH);
-      if (val.indexOf(MASTER) > -1) sms("RELAY ON OK", MASTER); // отвечаем смской
-      else sms("RELAY ON OK", MASTER2); // отвечаем смской
-      val = "";
-      state = true;
-      digitalWrite(LED, state);
-      EEPROM.update(1,state);
-      timer = 0;
-      EEPROM.update(2,1);
-    }
-    else if ((val.indexOf(MASTER) > -1 || val.indexOf(MASTER2) > -1) && val.indexOf("relay off") > -1 && state == true) //если СМС от хозяина и содержит текст запроса
-    {
-      digitalWrite(power, LOW);
-      if (val.indexOf(MASTER) > -1) sms("RELAY OFF OK", MASTER); // отвечаем смской
-      else sms("RELAY OFF OK", MASTER2); // отвечаем смской
-      val = "";
-      state = false;
-      digitalWrite(LED, state);
-      EEPROM.update(1,state);
-      timer = 0;
-      EEPROM.update(2,1);
-    }
-    
-    #ifdef USE_TIMER
-    else if ((val.indexOf(MASTER) > -1 || val.indexOf(MASTER2) > -1) && val.indexOf("timer ") > -1)
-      {
-      String timerTmp = val.substring(54);
-      timer = timerTmp.toInt();
-      //Serial.println(timer);
-      timerTmp = (String)timer;
-      timer = timer*60*1000+millis();
-      if(timer != 0) {
-        digitalWrite(power, HIGH);
-        state = true;
-        digitalWrite(LED, state);
-        EEPROM.update(1,state);
-        EEPROM.update(2,0);
-        if (val.indexOf(MASTER) > -1) sms("TIMER ON " + timerTmp + " MIN", MASTER); // отвечаем смской
-        else sms("TIMER ON " + timerTmp + " MIN", MASTER2); // отвечаем смской
-        }
-        val = "";
-      }
-    #endif
-
-    else if ((val.indexOf(MASTER) > -1 || val.indexOf(MASTER2) > -1) && val.indexOf("temper") > -1) //если СМС от хозяина и содержит текст запроса температуры
-    {
-      String message = (String)currentTemper();
-      if (val.indexOf(MASTER) > -1) sms("Temperature: " + message + "'C", MASTER); // в СМС отправляем текущую температуру
-      else sms("Temperature: " + message + "'C", MASTER2); // в СМС отправляем текущую температуру
-      val = "";
-    }
-
-    #ifdef USE_TERMOSTAT
-    else if ((val.indexOf(MASTER) > -1 || val.indexOf(MASTER2) > -1) && val.indexOf("termostat ") > -1)
-      {
-      String heatTmp = val.substring(58);
-      heaterVal = heatTmp.toInt();
-      //Serial.println(heaterVal);
-      EEPROM.update(3,heaterVal);
-      if(heaterVal != 0) {
-        String message = (String)heaterVal; // Массив для вывода
-        if (val.indexOf(MASTER) > -1) sms("TERMOSTAT ON " + message + "'C", MASTER); // отвечаем смской
-        else sms("TERMOSTAT ON " + message + "'C", MASTER2); // отвечаем смской
-        }
-        val = "";
-      }
-    #endif
-      
-    else if (val.indexOf("new master") > -1)
-      {
-      MASTER = val.substring(10, 21);
-      //Serial.println(MASTER);
-      update_master_eeprom(10);
-      sms("Master Nomer izmenen", MASTER); // отвечаем смской
-      val = "";
-      }
-
-    else if (val.indexOf("new master2") > -1)
-      {
-      MASTER2 = val.substring(10, 21);
-      //Serial.println(MASTER2);
-      update_master2_eeprom(30);
-      sms("Master Nomer izmenen", MASTER2); // отвечаем смской
-      val = "";
-      }
-
-    #ifdef USE_readNumberSIM
-    else if (val.indexOf("SIM master N") > -1)
-      {
-      val="";
-      readNumberSIM();
-      //Serial.println(MASTER);
-      sms("Master Nomer izmenen", MASTER); // отвечаем смской
-      }
-      #endif
-      
-    else
-    {
-      val = "";
-    }
-  }
-  else if (val.indexOf("RING") > -1)           //если обнаружен звонок
-  {
-    {
-      if (val.indexOf(MASTER) > -1 || val.indexOf(MASTER2) > -1)         //если звонок от хозяина
-
-      {
-        delay(500); // выждем, чтоб модем перешел в режим ожидания, иначе просадка напряжения возможна и перезагрузка
-        if (state == LOW)
-        {
-          digitalWrite(power, HIGH);
-          //delay(100);
-          //char message[15] = "RELAY ON OK"; // Массив для вывода
-          //sms(message, MASTER); // отвечаем смской
-          val = "";
-          delay(400);
-          state = true;
-          digitalWrite(LED, state);
-          EEPROM.update(1,state);
-          timer = 0;
-          EEPROM.update(2,1);
-        }
-        else if (state == HIGH)
-        {
-          digitalWrite(power, LOW);
-          //delay(100);
-          //char message[15] = "RELAY OFF OK"; // Массив для вывода
-          //sms(message, MASTER); // отвечаем смской
-          val = "";
-          delay(400);
-          state = false;
-          digitalWrite(LED, state);
-          EEPROM.update(1,state);
-          timer = 0;
-          EEPROM.update(2,1);
-        }
-        sendAtCmd("ATH0");     // сбросить вызов
-
-      }
-      else
-      {
-        delay(10500);
-        sendAtCmd("ATH");
-      }
-    }
-  }
-  val = "";
+    if (val.indexOf("RING") > -1) return CMD_RING;
+    return CMD_UNKNOWN;
 }
 
+void incoming_call_sms() {
+    byte ch = 0;
+    delay(200);
+    while (mySerial.available()) {
+        ch = mySerial.read();
+        val += char(ch);
+        delay(10);
+    }
 
-
+    Command cmd = getCommand(val);
+    switch (cmd) {
+        case CMD_DELETE_SMS:
+            sms("Delete SMS OK", (val.indexOf(MASTER) > -1) ? MASTER : MASTER2);
+            val = "";
+            delay(1000);
+            sendAtCmd("AT+CMGD=1,4");
+            break;
+        case CMD_RELAY_ON:
+            digitalWrite(power, HIGH);
+            sms("RELAY ON OK", (val.indexOf(MASTER) > -1) ? MASTER : MASTER2);
+            state = true;
+            digitalWrite(LED, state);
+            EEPROM.update(1, state);
+            timer = 0;
+            EEPROM.update(2, 1);
+            break;
+        case CMD_RELAY_OFF:
+            digitalWrite(power, LOW);
+            sms("RELAY OFF OK", (val.indexOf(MASTER) > -1) ? MASTER : MASTER2);
+            state = false;
+            digitalWrite(LED, state);
+            EEPROM.update(1, state);
+            timer = 0;
+            EEPROM.update(2, 1);
+            break;
+#ifdef USE_TIMER
+        case CMD_TIMER:
+            {
+                String timerTmp = val.substring(54);
+                timer = timerTmp.toInt() * 60000 + millis();
+                if (timer != 0) {
+                    digitalWrite(power, HIGH);
+                    state = true;
+                    digitalWrite(LED, state);
+                    EEPROM.update(1, state);
+                    EEPROM.update(2, 0);
+                    sms("TIMER ON " + timerTmp + " MIN", (val.indexOf(MASTER) > -1) ? MASTER : MASTER2);
+                }
+                val = "";
+            }
+            break;
+#endif
+        case CMD_TEMPERATURE:
+            sms("Temperature: " + String(currentTemper()) + "'C", (val.indexOf(MASTER) > -1) ? MASTER : MASTER2);
+            break;
+#ifdef USE_TERMOSTAT
+        case CMD_TERMOSTAT:
+            {
+                String heatTmp = val.substring(58);
+                heaterVal = heatTmp.toInt();
+                EEPROM.update(3, heaterVal);
+                if (heaterVal != 0) {
+                    sms("TERMOSTAT ON " + String(heaterVal) + "'C", (val.indexOf(MASTER) > -1) ? MASTER : MASTER2);
+                }
+                val = "";
+            }
+            break;
+#endif
+        case CMD_NEW_MASTER:
+            MASTER = val.substring(10, 21);
+            update_master_eeprom(10);
+            sms("Master Nomer izmenen", MASTER);
+            break;
+        case CMD_NEW_MASTER2:
+            MASTER2 = val.substring(10, 21);
+            update_master2_eeprom(30);
+            sms("Master Nomer izmenen", MASTER2);
+            break;
+#ifdef USE_readNumberSIM
+        case CMD_SIM_MASTER:
+            val = "";
+            readNumberSIM();
+            sms("Master Nomer izmenen", MASTER);
+            break;
+#endif
+        case CMD_RING:
+            if (val.indexOf(MASTER) > -1 || val.indexOf(MASTER2) > -1) {
+                delay(500);
+                state = !state;
+                digitalWrite(power, state ? HIGH : LOW);
+                digitalWrite(LED, state);
+                EEPROM.update(1, state);
+                timer = 0;
+                EEPROM.update(2, 1);
+                sendAtCmd("ATH0");
+            } else {
+                delay(10500);
+                sendAtCmd("ATH");
+            }
+            break;
+        default:
+            val = "";
+            break;
+    }
+}
